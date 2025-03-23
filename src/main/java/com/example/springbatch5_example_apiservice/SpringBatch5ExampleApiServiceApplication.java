1fc456c656791f012fed7c04df956fb022e3728a
subCharacters <wldns@naver.com>
2025-03-23 17:51:36 +0900

    initial commit

---
+++ src/main/java/com/example/springbatch5_example_apiservice/SpringBatch5ExampleApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch5_example_apiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatch5ExampleApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatch5ExampleApiServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbatch5_example_apiservice/SpringBatch5ExampleApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatch5_example_apiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatch5ExampleApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
